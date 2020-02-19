be89b74ee86163541ee349beb8b9480f39134e06
conejitomaya <eduardobonillac@gmail.com>
2020-02-19 16:26:33 -0600

    rubocoop lint
    fix

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -19,11 +19,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test 'should create user' do
     assert_difference('User.count') do
-      post users_url, params: { user:
-        {
+      post users_url, params: {
+        user: {
           email: @user.email,
           password: @user.password,
-          username: @user.username }
+          username: @user.username
+          }
         }
     end
 
@@ -41,9 +42,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should update user' do
-    patch user_url(@user), params: { user: { email: @user.email,
-      password: @user.password,
-      username: @user.username } }
+    patch user_url(@user), params: {
+      user: {
+        email: @user.email,
+        password: @user.password,
+        username: @user.username
+        }
+      }
     assert_redirected_to user_url(@user)
   end
 
