4e9fc21ebfb12220dff5249bd9cc184b5b59170b
conejitomaya <eduardobonillac@gmail.com>
2020-02-19 16:22:57 -0600

    rubocop lint error fixed controller.test-user_controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -19,7 +19,12 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test 'should create user' do
     assert_difference('User.count') do
-      post users_url, params: { user: { email: @user.email, password: @user.password, username: @user.username } }
+      post users_url, params: { user:
+        {
+          email: @user.email,
+          password: @user.password,
+          username: @user.username }
+        }
     end
 
     assert_redirected_to user_url(User.last)
@@ -36,7 +41,9 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should update user' do
-    patch user_url(@user), params: { user: { email: @user.email, password: @user.password, username: @user.username } }
+    patch user_url(@user), params: { user: { email: @user.email,
+      password: @user.password,
+      username: @user.username } }
     assert_redirected_to user_url(@user)
   end
 
